944978a6ebc3eb7f3d7261f572d8a687d1a83f76
claudimar <claudimards@gmail.com>
2021-01-04 11:04:09 -0300

    Retirado importação do atributo Link

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Footer from '../components/footer/Footer'
 import Header from '../components/header/Header'
 import './Work.css'
