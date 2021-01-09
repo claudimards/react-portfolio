54ed58744bf3da3310540cadc71b17f80a43ec6e
claudimar <claudimards@gmail.com>
2021-01-08 21:33:56 -0300

    Adicionado listener que define qual dado retornar ao navegador

--- public/sw.js
+++ public/sw.js
@@ -14,3 +14,14 @@ self.addEventListener('install', event => {
         })
     )
 })
+
+// listen for requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then( () => {
+            return fetch(event.request)
+            .catch( () => caches.match('offline.html'))
+        })
+    )
+})
