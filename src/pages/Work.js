28853d7b6ef55c113778d7011cbe62f0470e062c
claudimar <claudimards@gmail.com>
2021-03-06 18:57:25 -0300

    Adicionado novos links de serviços realizados

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -15,62 +15,62 @@ function Work() {
     return (
         <section className="work section" id="work">
                 <h2 className="section-title">Work</h2>
-                <p className="about__text" >Projetos aos quais tenho trabalhado</p>
+                <p className="about__text" >Últimos projetos aos quais tenho trabalhado</p>
                 <div className="work__container bd-grid">
                     <div>
-                        <Link to={'//homolog-php.nairuz.com.br/coffee/'} target="_blank">
+                        <Link to={'//coffeelover.com.br'} target="_blank">
                             <div className="work__img">
-                                <img src="./images/coffee-lover.webp" alt="Imagem de trabalho realizado" />
+                                <img src="./images/works/coffee-lover.webp" alt="Imagem de trabalho realizado" />
                             </div>
                             <h4>Coffee Lover</h4>
-                            <p className="about__text" >Projeto de blog, desenvolvido em Wordpress</p>
+                            <p className="about__text" >Site blog, projeto realizado com HTML5, CSS3, BS, JQUERY em WORDPRESS</p>
                         </Link>
                     </div>
                     
                     <div>
-                        <Link to={'//#'} target="_blank">
+                        <Link to={'//nairuz.com.br/servicos/'} target="_blank">
                             <div className="work__img">
-                                <img src="./images/work3.webp" alt="Imagem de trabalho realizado" />
+                                <img src="./images/works/nairuz-servicos.webp" alt="Imagem de trabalho realizado" />
                             </div>
-                            <h4>Job title</h4>
-                            <p className="about__text" ></p>
+                            <h4>Redesign da página de serviços</h4>
+                            <p className="about__text" >Re-estruturação de página, projeto realizado com HTML5, CSS3, BS, JQUERY em WORDPRESS</p>
                         </Link>
                     </div>
                     <div>
-                        <Link to={'//#'} target="_blank">
+                        <Link to={'//suporte.nairuz.com.br'} target="_blank">
                             <div className="work__img">
-                                <img src="./images/work3.webp" alt="Imagem de trabalho realizado" />
+                                <img src="./images/works/nairuz-suporte.webp" alt="Imagem de trabalho realizado" />
                             </div>
-                            <h4>Job title</h4>
-                            <p className="about__text" ></p>
+                            <h4>Site de Help Suporte</h4>
+                            <p className="about__text" >Redesign completo do site, projeto realizado com HTML5, CSS3, BS, JQUERY em WORDPRESS</p>
                         </Link>
                     </div>
                     <div>
-                        <Link to={'//#'} target="_blank">
+                        <Link to={'//blog.naturaloil.com.br'} target="_blank">
                             <div className="work__img">
-                                <img src="./images/work3.webp" alt="Imagem de trabalho realizado" />
+                                <img src="./images/works/natural-oil.webp" alt="Imagem de trabalho realizado" />
                             </div>
-                            <h4>Job title</h4>
-                            <p className="about__text" ></p>
+                            <h4>Natural Oil</h4>
+                            <p className="about__text" >Site blog, projeto realizado com HTML5, CSS3, BS, JQUERY em WORDPRESS</p>
                         </Link>
                     </div>
                     <div>
-                        <Link to={'//#'} target="_blank">
+                        <Link to={'//www.widestock.com.br'} target="_blank">
                             <div className="work__img">
-                                <img src="./images/work3.webp" alt="Imagem de trabalho realizado" />
+                                <img src="./images/works/widestock.webp" alt="Imagem de trabalho realizado" />
                             </div>
-                            <h4>Job title</h4>
-                            <p className="about__text" ></p>
+                            <h4>WideStock</h4>
+                            <p className="about__text" >Site de e-commerce, projeto realizado com HTML5, CSS3, BS, JQUERY com MAGENTO</p>
                         </Link>
                     </div>
 
                     <div>
                         <Link to={'//grassycafe.mageshop.com.br/'} target="_blank">
                             <div className="work__img">
-                                <img src="./images/grassy-caffe.webp" alt="Imagem de trabalho realizado" />
+                                <img src="./images/works/grassy-caffe.webp" alt="Imagem de trabalho realizado" />
                             </div>
                             <h4>Grassy Caffè</h4>
-                            <p className="about__text" >Projeto de ecommerce, desenvolvido em HTML5, CSS3, JS, JQUERY e MAGENTO</p>
+                            <p className="about__text" >Site de e-commerce, projeto realizado com HTML5, CSS3, BS, JQUERY com MAGENTO</p>
                         </Link>
                     </div>
                 </div>
