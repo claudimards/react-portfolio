e278a24c39cd65d6faf49da633d72a694e783053
claudimar <claudimards@gmail.com>
2021-03-07 00:30:00 -0300

    Implementado funcionalidade do form de contato, com email-js

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
 import { Link } from 'react-router-dom'
 import './Contact.css'
 import sr from '../../components/scrollreveal/ScrollReveal'
 
 function Contact() {
+
     const config = {
         origin: 'top',
         distance: '80px',
@@ -12,22 +14,58 @@ function Contact() {
     }
     sr.reveal('.contact__input', {interval: 200}, config)
 
+    const sendEmail = (e) => {
+        const loaderIcon = document.querySelector( '.loader' )
+        loaderIcon.classList.toggle( 'active-load' )
+        
+        const status = document.querySelector( '.status' )
+
+        e.preventDefault();
+    
+        emailjs.sendForm('email-js-service', 'adevindev-site', e.target, 'user_BplUStcvILWFOcGjxH42G')
+          .then((result) => {
+              //console.log(result.text);
+              e.target.reset()
+              loaderIcon.classList.toggle( 'active-load' )
+              status.classList.toggle( 'success' )
+              setInterval(() => {
+                status.classList.add( 'fade' )
+              }, 500);
+          }, (error) => {
+              //console.log(error.text);
+              loaderIcon.classList.toggle( 'active-load' )
+              status.classList.toggle( 'error' )
+              setInterval(() => {
+                status.classList.add( 'fade' )
+              }, 500);
+          });
+    }    
+
     return (
         <section className="contact section" id="contact">
                 <h2 className="section-title">Contact</h2>
 
-                <h3 className="warning"><i className='bx bx-tired'></i>Contact form out of work, still in development</h3>
+                <h4 className="wpp-info"><Link to={'//api.whatsapp.com/send?l=pt_BR&phone=5519981828929'} target="_blank" className=""><i className='bx bxl-whatsapp' ></i>Clique aqui e mande uma mensagem WhatsApp</Link></h4>
+                <h3 className="wpp-info">Ou se preferir</h3>
 
-                <h4 className="wpp-info"><Link to={'//api.whatsapp.com/send?l=pt_BR&phone=5519981828929'} target="_blank" className=""><i className='bx bxl-whatsapp' ></i>Click here and get in touch via WhatsApp</Link></h4>
+                <h3 className="wpp-info">Entre em contato por e-mail</h3>
 
                 <div className="contact__container bd-grid">
-                    <form action="#" className="contact__form">
-                        <input name="name" type="text" placeholder="Name" className="contact__input" />
-                        <input name="email" type="email" placeholder="Email" className="contact__input" />
-                        <input name="subject" type="text" placeholder="Subject" className="contact__input" />
-                        <textarea name="message" id="message" cols="0" rows="10" className="contact__input" placeholder="Message..."></textarea>
+                    <form onSubmit={sendEmail} className="contact__form">
+                        <input name="name" type="text" placeholder="Nome" className="contact__input" required />
+                        <input name="email" type="email" placeholder="Email" className="contact__input" required />
+                        <input name="subject" type="text" placeholder="Assunto" className="contact__input" required />
+                        <textarea name="message" id="message" cols="0" rows="10" className="contact__input" placeholder="Mensagem..." required></textarea>
 
-                        <input type="button" value="Send" className="contact__button button" />
+                        <div className="loader">
+                            <i className='bx bx-loader-circle' ></i>
+                        </div>
+                        <input type="submit" value="Enviar" className="contact__button button" />
+
+                        <div className="status" hidden>
+                            <p className="status-text-success"><i className='bx bx-check-double'></i>E-mail enviado</p>
+                            <p className="status-text-error"><i className='bx bx-error-circle'></i>E-mail não enviado</p>
+                        </div>
                     </form>
                 </div>
             </section>
